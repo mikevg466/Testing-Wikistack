96ecbb56470985945157fbdece9e840af608c02f
Mike van Genderen <mikevg466>
2017-05-02 12:57:01 -0400

    Starts page model test specs

---
+++ test/page.model.spec.js
@@ -0,0 +1,48 @@
+describe("Models", function(){
+
+  describe("Create Page Database", function(){
+
+    it("has 5 given columns: title, urlTitle, content, status, and tags", function(){
+
+    });
+
+    it("Does not allow null for title, urlTitle, and content", function(){
+
+    });
+
+    it("", function(){
+
+    });
+  });
+
+    describe("", function(){
+
+      it("", function(){
+
+      });
+
+      it("", function(){
+
+      });
+
+      it("", function(){
+
+      });
+    });
+
+      describe("", function(){
+
+        it("", function(){
+
+        });
+
+        it("", function(){
+
+        });
+
+        it("", function(){
+
+        });
+      });
+
+});
